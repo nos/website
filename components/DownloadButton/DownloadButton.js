925f7267661ea68919a9e01cc2597f718046164d
Matt Huggins <matt.huggins@gmail.com>
2018-10-19 21:04:31 -0500

    feat(next): add download modal

--- components/DownloadButton/DownloadButton.js
+++ components/DownloadButton/DownloadButton.js
@@ -1,13 +1,26 @@
+import classNames from 'classnames';
+import { string } from 'prop-types';
+
 import styles from './DownloadButton.scss';
 
 const DownloadButton = (props) => (
   <a
-    className={`button ${styles.downloadButton}`}
+    className={classNames('button', styles.downloadButton, props.className)}
     href="https://github.com/nos/client/releases"
     target="_blank"
   >
-    Download Now
+    {props.label}
   </a>
 );
+
+DownloadButton.propTypes = {
+  className: string,
+  label: string
+};
+
+DownloadButton.defaultProps = {
+  className: null,
+  label: 'Download Now'
+};
 
 export default DownloadButton;
