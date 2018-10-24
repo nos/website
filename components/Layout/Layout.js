e0924b80b35106994e9f8f2ce519a6724c3b7739
Dean van Dugteren <hello@dean.press>
2018-10-24 19:56:26 +0200

    Add favicon

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -36,6 +36,7 @@ export default class Layout extends React.Component {
           <meta httpEquiv="x-ua-compatible" content="ie=edge" />
           <meta name="description" content="" />
           <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+          <link rel="shortcut icon" href="static/logo-nos.png" type="image/x-icon" />
           <title>{this.props.title}</title>
         </Head>
 
