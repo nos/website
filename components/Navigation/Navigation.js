2b905863f360868f4132a7c219e4453dd7e9747c
Dean van Dugteren <hello@dean.press>
2019-01-26 19:41:54 +0100

    chore(nav): Remove "Token Sale" from header

--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -8,8 +8,7 @@ const Navigation = (props) => (
   <div className={classNames(styles.navigation, { [styles.fixed]: props.fixed })}>
     <Link href="/"><a className={styles.logo} /></Link>
     <nav className={styles.navItems}>
-        <Link href="/#hero"><a>Download</a></Link>
-        <Link href="/#tge"><a>Token Sale</a></Link>
+      <Link href="/#hero"><a>Download</a></Link>
       <Link href="/#features"><a>Features</a></Link>
       <Link href="/#showcases"><a>Showcases</a></Link>
       {/* <Link href="/#demo"><a>Create</a></Link> */}
