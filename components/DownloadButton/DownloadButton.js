379bce453498fdb0f53e34b02d8cbd00de21797d
Jeroen Peeters <jeroen.peeters@evance.be>
2019-02-27 13:56:36 +0100

    chore(downloadButton): pass downloadUrl via context

--- components/DownloadButton/DownloadButton.js
+++ components/DownloadButton/DownloadButton.js
@@ -1,52 +1,31 @@
 import classNames from 'classnames';
 import {string} from 'prop-types';
 import styles from './DownloadButton.scss';
+import { Consumer } from '../Layout/Context';
 
-export default class DownloadButton extends React.Component {
+const DownloadButton = ({ className, label }) => (
+  <Consumer>
+    {downloadUrl => console.log(downloadUrl) || (
+      <a
+        className={classNames('button', styles.downloadButton, className)}
+        href={downloadUrl}
+        target="_blank"
+      >
+        {label}
+      </a>
+    )}
+  </Consumer>
+);
 
-    state = {downloadUrl: "https://github.com/nos/client/releases"};
-
-    setDownloadUrl = async () => {
-        const resp = await fetch('https://api.github.com/repos/nos/client/releases/latest');
-        const response = await resp.json();
-        const assets = response.assets;
-        assets.forEach(asset => {
-            const browserDownloadUrl = asset.browser_download_url;
-            if ((browserDownloadUrl.endsWith(".exe") && navigator.platform.indexOf('Win') !== -1) || (browserDownloadUrl.endsWith(".dmg") && navigator.platform.indexOf('Mac') !== -1) || (browserDownloadUrl.endsWith(".AppImage") && navigator.platform.indexOf('Linux') !== -1)) {
-                this.setState({downloadUrl : browserDownloadUrl});
-            }
-        })
-    }
-
-    componentDidMount() {
-            this.setDownloadUrl();
-    }
-
-
-    render() {
-        return (
-            <a
-                className={classNames('button', styles.downloadButton, this.props.className)}
-                href={this.state.downloadUrl}
-                target="_blank"
-            >
-                {this.props.label}
-            </a>
-        )
-    }
-
-}
 
 DownloadButton.propTypes = {
-    className: string,
-    label: string
+  className: string,
+  label: string
 }
 
 DownloadButton.defaultProps = {
-    className: null,
-    label: 'Download Now'
+  className: null,
+  label: 'Download Now'
 }
 
-
-
-
+export default DownloadButton;
