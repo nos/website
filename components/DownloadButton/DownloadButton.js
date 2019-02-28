fda4c07e91eba04e8bbea99937aad30096b668f4
Maurice Dalderup <Maurice_Dalderup@hotmail.com>
2019-02-28 21:28:36 +0100

    chore(starGazer): move to nav

--- components/DownloadButton/DownloadButton.js
+++ components/DownloadButton/DownloadButton.js
@@ -5,23 +5,13 @@ import { Consumer } from '../Layout/Context';
 
 import styles from './DownloadButton.scss';
 
-const DownloadButton = ({ className, label, starGazer }) => (
+const DownloadButton = ({ className, label }) => (
   <Consumer>
     {downloadUrl => (
       <React.Fragment>
         <a className={classNames('button', styles.downloadButton, className)} href={downloadUrl}>
           {label}
         </a>
-        {starGazer && (
-          <iframe
-            className={styles.starGazer}
-            src="https://ghbtns.com/github-btn.html?user=nos&repo=client&type=star&count=true&size=large"
-            frameborder="0"
-            scrolling="0"
-            width="160px"
-            height="30px"
-          />
-        )}
       </React.Fragment>
     )}
   </Consumer>
@@ -29,14 +19,12 @@ const DownloadButton = ({ className, label, starGazer }) => (
 
 DownloadButton.propTypes = {
   className: string,
-  label: string,
-  starGazer: bool
+  label: string
 };
 
 DownloadButton.defaultProps = {
   className: null,
-  label: 'Download Now',
-  starGazer: false
+  label: 'Download Now'
 };
 
 export default DownloadButton;
--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -4,15 +4,33 @@ import { bool } from 'prop-types';
 
 import styles from './Navigation.scss';
 
-const Navigation = (props) => (
+const Navigation = props => (
   <div className={classNames(styles.navigation, { [styles.fixed]: props.fixed })}>
-    <Link href="/"><a className={styles.logo} /></Link>
+    <Link href="/">
+      <a className={styles.logo} />
+    </Link>
     <nav className={styles.navItems}>
-      <Link href="/#hero"><a>Download</a></Link>
-      <Link href="/#features"><a>Features</a></Link>
-      <Link href="/#showcases"><a>Showcases</a></Link>
+      <Link href="/#hero">
+        <a>Download</a>
+      </Link>
+      <Link href="/#features">
+        <a>Features</a>
+      </Link>
+      <Link href="/#showcases">
+        <a>Showcases</a>
+      </Link>
       {/* <Link href="/#demo"><a>Create</a></Link> */}
-      <Link href="/#team"><a>Team</a></Link>
+      <Link href="/#team">
+        <a>Team</a>
+      </Link>
+      <iframe
+        className={styles.starGazer}
+        src="https://ghbtns.com/github-btn.html?user=nos&repo=client&type=star&size=large"
+        frameborder="0"
+        scrolling="0"
+        width="160px"
+        height="30px"
+      />
     </nav>
   </div>
 );
