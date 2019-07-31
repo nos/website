baa4c20cb1ba7b3e654d592587159f6c9d8c5ca4
Jeroen Peeters <contact@jeroenpeeters.me>
2019-08-01 00:01:44 +0200

    chore: update team info

--- components/Team/Team.js
+++ components/Team/Team.js
@@ -1,26 +1,27 @@
-import Section from '../Section';
-import Container from '../Container';
-import Intro from '../Intro';
-import Card from './Card';
-import team from '../../data/team.json';
-import styles from './Team.scss';
+import Section from "../Section";
+import Container from "../Container";
+import Intro from "../Intro";
+import Card from "./Card";
+import team from "../../data/team.json";
+import styles from "./Team.scss";
 
 const Team = () => (
   <Section className={styles.team}>
     <Container>
       <a name="team" />
 
-      <Intro>
+      <Intro className={styles.teamHero}>
         <h2>Team</h2>
         <p className={styles.large}>
-          The nOS team consists of experienced developers and researchers with a history in blockchain and smart contract development.
-          With roots in City of Zion, many members of the team have developed on (and for) the NEO Blockchain.
-            {/*{' '}*/}
+          The nOS team consists of experienced developers and researchers with a history in blockchain and smart
+          contract development. With roots in City of Zion, many members of the team have developed on (and for) the NEO
+          Blockchain.
+          {/*{' '}*/}
         </p>
       </Intro>
 
       <div className={styles.cards}>
-        {team.map((member) => (
+        {team.map(member => (
           <Card key={member.name} {...member} />
         ))}
       </div>
